95719b6b818b1e96f9fa46326d2597f976564fe5
2Tucho <albergromero@gmail.com>
2024-05-08 16:31:26 +0200

    add fetch function in Search

--- client/src/components/Header/Logged/Logged.jsx
+++ client/src/components/Header/Logged/Logged.jsx
@@ -6,7 +6,7 @@ const Logged = () => {
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/collection">Collection</Link></li>
-      <li><Link to="/">Logout</Link></li>
+      <li><Link to="#">Logout</Link></li>
     </ul>
     
     <div>
--- client/src/components/Main/Home/Search/Search.jsx
+++ client/src/components/Main/Home/Search/Search.jsx
@@ -1,54 +1,141 @@
-import React from "react";
+import { useState, useRef, useEffect } from "react";
+import { debounce } from "lodash";
+import axios from 'axios';
 
-const Search = () => {
+const Search = ({ setSearchedData }) => {
+  const [inputCard, setInputCard] = useState("");
+  const inputRef = useRef();
+  const formatRef = useRef();
+  const supertypeRef = useRef();
+  const orderRef = useRef();
+
+  useEffect(() => {
+    let filterSupertypes = "";
+    let filterFormat = "";
+    let orderBy = "";
+
+    if (supertypeRef.current.value == "Pokémon") {
+      filterSupertypes = "supertype:pokemon"
+    } else if (supertypeRef.current.value == "Trainers") {
+      filterSupertypes = "supertype:trainer"
+    } else if (supertypeRef.current.value == "Energies") {
+      filterSupertypes = "supertype:energy"
+    } else filterSupertypes = "";
+
+    if (formatRef.current.value == "Standard") {
+      filterFormat = "legalities.standard:legal"
+    } else if (formatRef.current.value == "Expanded") {
+      filterFormat = "legalities.expanded:legal"
+    } else if (formatRef.current.value == "Unlimited") {
+      filterFormat = "legalities.unlimited:legal"
+    } else filterFormat = "legalities.unlimited:legal";
+
+    if (orderRef.current.value == "Released ↓") {
+      orderBy = "orderBy=-set.releaseDate"
+    } else if (orderRef.current.value == "Updated ↓") {
+      orderBy = "orderBy=-set.updatedAt"
+    } else if (orderRef.current.value == "Released ↑") {
+      orderBy = "orderBy=set.releaseDate"
+    } else if (orderRef.current.value == "Updated ↑") {
+      orderBy = "orderBy=set.updatedAt"
+    } else if (orderRef.current.value == "HP ↓") {
+      orderBy = "orderBy=-hp"
+    } else if (orderRef.current.value == "Retreat Cost ↓") {
+      orderBy = "orderBy=-convertedRetreatCost"
+    } else if (orderRef.current.value == "HP ↑") {
+      orderBy = "orderBy=hp"
+    } else if (orderRef.current.value == "Retreat Cost ↑") {
+      orderBy = "orderBy=convertedRetreatCost"
+    } else if (orderRef.current.value == "Rarity ↓") {
+      orderBy = "orderBy=-rarity"
+    } else if (orderRef.current.value == "Rarity ↑") {
+      orderBy = "orderBy=rarity"
+    } else if (orderRef.current.value == "Set Name ↓") {
+      orderBy = "orderBy=-set.name"
+    } else if (orderRef.current.value == "Set Name ↑") {
+      orderBy = "orderBy=set.name"
+    } else if (orderRef.current.value == "Type ↓") {
+      orderBy = "orderBy=-types"
+    } else if (orderRef.current.value == "Type ↑") {
+      orderBy = "orderBy=types"
+    } else orderBy = "orderBy=-set.releaseDate"
+
+    function fetchData() {
+      const url = `https://api.pokemontcg.io/v2/cards?q=name:*${inputCard}*%20${filterSupertypes}%20${filterFormat}&${orderBy}`;
+      const headers = {
+        "X-Api-Key": import.meta.env.VITE_API_KEY,
+      };
+      try {
+        axios.get(url, { headers })
+          .then(response => setSearchedData(response))
+          .catch(error => {
+            console.error('There was an error!', error);
+          });
+        console.log(url);
+        console.log(orderRef.current.value)
+        console.log(formatRef.current.value)
+        console.log(supertypeRef.current.value)
+        console.log(printData.data.data[0].types[0])
+      } catch {
+        console.log("ERROR: NOT FOUND")
+      }
+    };
+    fetchData();
+  }, [inputCard])
+
+  function searchCard() {
+    setInputCard(inputRef.current.value)
+    console.log(inputCard)
+  }
+
+  const debouncedOnChange = debounce(searchCard, 1500)
+
   return <div className="search">
     <div>
       <p>Order by...</p>
-      <select className="order-by">
-
+      <select className="order-by" ref={orderRef} defaultValue="Released ↓">
         <optgroup label="Date">
-          <option>Added ↓</option>
-          <option selected>Released ↓</option>
+          <option>Released ↓</option>
           <option>Updated ↓</option>
-          <option>Added ↑</option>
           <option>Released ↑</option>
           <option>Updated ↑</option>
         </optgroup>
+
         <optgroup label="Characteristics">
           <option>HP ↓</option>
           <option>Retreat Cost ↓</option>
-          <option>Set Name ↓</option>
           <option>HP ↑</option>
           <option>Retreat Cost ↑</option>
-          <option>Set Name ↑</option>
         </optgroup>
+
         <optgroup label="Other">
-          <option>Name ↓</option>
           <option>Rarity ↓</option>
+          <option>Set Name ↓</option>
           <option>Type ↓</option>
-          <option>Name ↑</option>
           <option>Rarity ↑</option>
+          <option>Set Name ↑</option>
           <option>Type ↑</option>
         </optgroup>
       </select>
 
       <p>Filter by...</p>
-      <select className="filter-by-format">
-        <option disabled selected>-- Format --</option>
-        <option value="Standard">Standard</option>
-        <option value="Expanded">Expanded</option>
-        <option value="Unlimited">Unlimited</option>
-      </select>
-      <select className="filter-by-supertype">
-        <option disabled selected>-- Supertype --</option>
+      <select className="filter-by-supertype" ref={supertypeRef} defaultValue="--Supertype--">
+        <option disabled>--Supertype--</option>
         <option value="Pokémon">Pokémon</option>
         <option value="Trainers">Trainers</option>
         <option value="Energies">Energies</option>
       </select>
+
+      <select className="filter-by-format" ref={formatRef} defaultValue="--Format--">
+        <option disabled>--Format--</option>
+        <option value="Standard">Standard</option>
+        <option value="Expanded">Expanded</option>
+        <option value="Unlimited">Unlimited</option>
+      </select>
     </div>
 
     <form action="#">
-      <input type="search" autoFocus />
+      <input type="search" ref={inputRef} onChange={debouncedOnChange} autoFocus />
       <button>Search</button>
     </form>
   </div>;
