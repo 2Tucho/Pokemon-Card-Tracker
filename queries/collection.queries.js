f9f6c400a9b2126d7692c408bc77a92b6fcf7c97
2Tucho <albergromero@gmail.com>
2024-05-12 11:54:42 +0200

    add sql queries

--- queries/collection.queries.js
+++ queries/collection.queries.js
@@ -1,20 +1,27 @@
 const queries = {
+
     getAllCards: `
-    SELECT * FROM collection
+        SELECT * FROM collection as c
+        INNER JOIN users_collection as uc ON c.id = uc.poke_id
+        WHERE uc.user_id = 1
+            `,
+    postCard: `
+        INSERT INTO collection (id, name, number, set, img_url, normal_foil, holo_foil, reverse_foil, play_pokemon, play_pokemon_foil)
+        VALUES ('sv4pt5-232', 'Mew ex', 232, 'Paldean Fates','https://images.pokemontcg.io/sv4pt5/232.png', 0, 0, 0, 0, 0)
         `,
-    /* createAuthor: `INSERT INTO authors(name,surname,email,image) 
-    VALUES ($1,$2,$3,$4)`,
-    updateAuthor: `UPDATE authors
-        SET
-        name=$1, 
-        surname=$2, 
-        email=$3, 
-        image=$4,
-        WHERE 
-        email=$5
-        `,
-    deleteAuthor: `DELETE FROM authors
-    WHERE email=$1
-        ` */
+    updateCard: `UPDATE collection
+        SET 
+        normal_foil = 4, 
+        holo_foil = 2, 
+        reverse_foil = 1, 
+        play_pokemon = 2, 
+        play_pokemon_foil = 0
+        WHERE id = 'sv4pt5-232';
+            `,
+    deleteCard: `DELETE FROM collection 
+    WHERE id = 'sv4pt5-232'
+        `
+        
 };
+
 module.exports = queries;
